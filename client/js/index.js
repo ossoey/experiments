9b1b712d0050b5656b53e85240c1e406cdb9958b
J__J <ebanga@hotmail.com>
2023-05-26 10:12:43 -0400

    Add files via upload

---
+++ client/js/index.js
@@ -0,0 +1,11 @@
+
+let header = {},
+    footer = {};
+    home = {};
+
+document.addEventListener("DOMContentLoaded", function() {   
+  headerIni(header);
+  homeIni(home);
+  footerIni(footer);
+ 
+});
